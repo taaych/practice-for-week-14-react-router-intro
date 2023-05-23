b31f1ed95757cbb50a16de783c2b33edcdb9307f
Tameka A <56846452+taaych@users.noreply.github.com>
2023-05-23 23:17:54 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+       <Route path='/movies'>
+        <Movies />
+      </Route>
+       <Route path='/stocks'>
+        <Stocks />
+      </Route>
+      <Route>
+        <h1>Page Not Found</h1>
+      </Route>
+      </Switch>
     </div>
   );
 }
